ab9a648d610cd72e2da36293eac84a702addae0b
leryeme17 <leryeme.benmakhlouf@ensia.edu.dz>
2026-01-07 02:39:50 +0100

    Update

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -48,7 +48,7 @@ export const Chatbot = () => {
       console.log("Contents:", contents);
 
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`,
         {
           method: 'POST',
           headers: {
